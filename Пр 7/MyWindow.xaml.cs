8ec9d12987aaf015aed96864ce6fe21ce84c2a96
ElizavetaKuz <100426901+ElizavetaKuz@users.noreply.github.com>
2022-03-13 18:48:59 +0300

    Add files via upload

---
+++ Пр 7/MyWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFThemes._7
+{
+    /// <summary>
+    /// Логика взаимодействия для MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+
+        public MyWindow()
+        {
+            InitializeComponent();
+            MainWindow wnd1 = null;
+            if (myWin == null) ;
+
+        }
+
+        private bool _close;
+        private MainWindow? wnd1;
+        private object myWin;
+
+        public new void Close()
+        {
+            _close = true;
+            base.Close();
+        }
+        private void Window_Closing_1(object sender,
+System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+                wnd1.txtBlock.Text = textBox.Text;
+            }
+            Close();
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1.myWin = null;
+        }
+
+
+    }
+
+
+
+}
