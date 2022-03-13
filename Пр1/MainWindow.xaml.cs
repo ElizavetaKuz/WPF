551173ae2539ad4971402c5881b3f3bcd9e08bc7
ElizavetaKuz <100426901+ElizavetaKuz@users.noreply.github.com>
2022-03-13 18:24:02 +0300

    Add files via upload

---
+++ Пр1/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDataDirty;
+        
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            lbl.Content = "Добрый день!";
+            set_But.IsEnabled = false;
+            retBut.IsEnabled = false;
+            Top = 25;
+            Left = 25;
+
+        }
+
+        private void SetBut_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new System.IO.StreamWriter("username.txt");
+                sw.WriteLine(setText_.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
+        }
+
+        private void retBut_Click(object sender, RoutedEventArgs e)
+        {
+
+            System.IO.StreamReader sr = null;
+            try
+            {
+                using (sr = new System.IO.StreamReader("username.txt"))
+                    retLabel_.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+        }
+       
+            private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            set_But.IsEnabled = true;
+            retBut.IsEnabled = true;
+            bool isDataDirty = false;
+            isDataDirty = false;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения ? ";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void New_Win_Click(object sender, RoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow(); 
+            myWin.Owner = this;
+          
+            myWin.Show();
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+
+        }
+        
+        public MyWindow myWin { get; set; }
+      
+    }
+  
+}
